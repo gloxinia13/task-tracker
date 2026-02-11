a0833fc88d4e61a351fe4092c67c27cfb1d43a1a
gloxinia13 <uzanzaev44@gmail.com>
2026-02-11 19:17:26 +0600

    Initial commit

---
+++ src/models/Status.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum Status {
+    todo,
+    in_progress,
+    done
+}
---
+++ src/models/Task.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.Date;
+
+public class Task {
+    private int id;
+    private String description;
+    private Date createdAt;
+    private Date updatedAt;
+}
